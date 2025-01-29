5ed925ba163709099ec7578fbedcd52de3682b85
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-01-29 19:13:32 +0530

    navbar for small screens done

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { IoMenuSharp, IoClose } from "react-icons/io5";
 
 const navLists = [
   { name: "Home", link: "/" },
@@ -9,6 +10,8 @@ const navLists = [
 ];
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
   return (
     <header className="bg-white py-6 border">
       <nav className="container mx-auto flex justify-between px-5">
@@ -18,13 +21,57 @@ const Navbar = () => {
         <ul className="sm:flex hidden items-center gap-8">
           {navLists.map((list, index) => (
             <li>
-              <NavLink to={list.link} className={({ isActive}) => isActive ? "active" : ""}>
-
-              {list.name}</NavLink>
+              <NavLink
+                to={list.link}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {list.name}
+              </NavLink>
             </li>
           ))}
+          <li>
+            <NavLink to="/login" className="btn btn-primary">
+              login
+            </NavLink>
+          </li>
         </ul>
+
+        {/* toggle Menu */}
+        <div className="flex items-center sm:hidden">
+          <button
+            onClick={toggleMenu}
+            className="flex items-center px-3 py-4 bg-[#fafafa] rounded text-sm text-gray-500 hover:text-gray-900"
+          >
+            {isMenuOpen ? (
+              <IoClose className="size-6" />
+            ) : (
+              <IoMenuSharp className="size-6" />
+            )}
+          </button>
+        </div>
       </nav>
+
+      {/* mobile menu items */}
+      {isMenuOpen && (
+        <ul className="fixed top-[108px] left-0 w-full h-auto pb-8 border-b bg-white shadow-sm z-50">
+          {navLists.map((list, index) => (
+            <li className="mt-5 px-4">
+              <NavLink
+                onClick={() => setIsMenuOpen(false)}
+                to={list.link}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {list.name}
+              </NavLink>
+            </li>
+          ))}
+          <li className="mt-5 px-4">
+            <NavLink to="/login" className="btn btn-primary">
+              login
+            </NavLink>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
